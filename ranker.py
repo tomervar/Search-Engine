f51b671e52258dda0d923aa9bffcfb1106e7895c
unknown <tomervaron1994@gmail.com>
2021-01-06 22:57:01 +0200

    its work (for now)

--- ranker.py
+++ ranker.py
@@ -1,8 +1,11 @@
-# you can change whatever you want in this module, just make sure it doesn't 
+import math
+
+# you can change whatever you want in this module, just make sure it doesn't
 # break the searcher module
 class Ranker:
-    def __init__(self):
-        pass
+    def __init__(self, indexer):
+        self.indexer = indexer
+
 
     @staticmethod
     def rank_relevant_docs(relevant_docs, k=None):
@@ -17,4 +20,32 @@ class Ranker:
         if k is not None:
             ranked_results = ranked_results[:k]
         return [d[0] for d in ranked_results]
+
+    def rank_tf_idf_query(self, query_as_dict, query_len):
+        query_term_weights_dict = {}
+        for term in query_as_dict:
+            tf = query_as_dict[term]/query_len
+            idf = self.indexer.inverted_idx[term][1]
+            w_iq = tf * idf
+            query_term_weights_dict[term] = w_iq
+        return query_term_weights_dict
+
+    def calculate_cos_sim(self, query_term_weights_dict, term_in_docs_tuple_list, doc_id):
+        inner_product = 0
+        segma_w_iq_pow = 0
+        for term in query_term_weights_dict:
+            w_iq = query_term_weights_dict[term]
+            for tuple_term in term_in_docs_tuple_list:
+                if tuple_term[0] == term:
+                    w_ij = tuple_term[1]
+                    inner_product += w_ij * w_iq
+                    break
+            segma_w_iq_pow += math.pow(w_iq, 2)
+
+        doc_sqrt_segma_wij_pow = self.indexer.weight_of_docs[doc_id]
+        sqrt_segma_w_iq_pow = math.sqrt(segma_w_iq_pow)
+
+        cos_sim_normalization = doc_sqrt_segma_wij_pow * sqrt_segma_w_iq_pow
+        cos_sim = inner_product/cos_sim_normalization
+        return cos_sim
 
--- searcher.py
+++ searcher.py
@@ -1,5 +1,11 @@
 from ranker import Ranker
 import utils
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import lin_thesaurus as thes
+from nltk import pos_tag
+# nltk.download('averaged_perceptron_tagger')
+# nltk.download('lin_thesaurus')
 
 
 # DO NOT MODIFY CLASS NAME
@@ -12,8 +18,38 @@ class Searcher:
     def __init__(self, parser, indexer, model=None):
         self._parser = parser
         self._indexer = indexer
-        self._ranker = Ranker()
+        self._ranker = Ranker(indexer)
         self._model = model
+        self.with_thesaurus = False
+        self.with_spelling_correction = False
+
+    def set_thesaurus(self):
+        self.with_thesaurus = True
+
+    def set_spelling_correction(self):
+        self.with_spelling_correction = True
+
+    def build_thesaurus_for_query(self, query_as_dict, query):
+        list_of_terms = list(query_as_dict.keys())
+        # list_of_terms = word_tokenize(query)
+        parts_of_speech_tags = pos_tag(list_of_terms)
+        for word, word_type in parts_of_speech_tags:
+            if word_type == "NN":
+                list_from_thes = list(thes.synonyms(word, fileid="simN.lsp"))
+            elif word_type == "VB":
+                list_from_thes = list(thes.synonyms(word, fileid="simV.lsp"))
+            elif word_type == "JJ":
+                list_from_thes = list(thes.synonyms(word, fileid="simA.lsp"))
+            else:
+                continue
+
+            if len(list_from_thes) > 0:
+                word_from_thes = list_from_thes[0]
+                if word_from_thes in self._indexer.inverted_idx:
+                    if word_from_thes in query_as_dict:
+                        query_as_dict[word_from_thes] += 0.5
+                    else:
+                        query_as_dict[word_from_thes] = 0.5
 
     # DO NOT MODIFY THIS SIGNATURE
     # You can change the internal implementation as you see fit.
@@ -30,24 +66,46 @@ class Searcher:
             and the last is the least relevant result.
         """
         query_as_list = self._parser.parse_sentence(query)
+        query_as_dict = {}
+        for term in query_as_list:
+            if term in query_as_dict:
+                query_as_dict[term] += 1
+            else:
+                query_as_dict[term] = 1
 
-        relevant_docs = self._relevant_docs_from_posting(query_as_list)
+        if self.with_thesaurus:
+            self.build_thesaurus_for_query(query_as_dict, query)
+        if self.with_spelling_correction:
+            pass
+
+        relevant_docs = self._relevant_docs_from_posting(query_as_dict, len(query_as_list))
         n_relevant = len(relevant_docs)
         ranked_doc_ids = Ranker.rank_relevant_docs(relevant_docs)
         return n_relevant, ranked_doc_ids
 
     # feel free to change the signature and/or implementation of this function 
     # or drop altogether.
-    def _relevant_docs_from_posting(self, query_as_list):
+    def _relevant_docs_from_posting(self, query_as_dict, query_len):
         """
         This function loads the posting list and count the amount of relevant documents per term.
         :param query_as_list: parsed query tokens
         :return: dictionary of relevant documents mapping doc_id to document frequency.
         """
         relevant_docs = {}
-        for term in query_as_list:
+        relevant_docs_with_weight = {}
+        query_term_weights_dict = self._ranker.rank_tf_idf_query(query_as_dict, query_len)
+        for term in query_term_weights_dict:
             posting_list = self._indexer.get_term_posting_list(term)
-            for doc_id, tf in posting_list:
-                df = relevant_docs.get(doc_id, 0)
-                relevant_docs[doc_id] = df + 1
+            for list_in_posting_list in posting_list:
+                doc_id = list_in_posting_list[0]
+                w_ij = list_in_posting_list[4]
+                if doc_id in relevant_docs_with_weight:
+                    relevant_docs_with_weight[doc_id].append((term, w_ij))
+                else:
+                    relevant_docs_with_weight[doc_id] = [(term, w_ij)]
+
+        for doc_id in relevant_docs_with_weight:
+            cos_sim = self._ranker.calculate_cos_sim(query_term_weights_dict, relevant_docs_with_weight[doc_id], doc_id)
+            relevant_docs[doc_id] = cos_sim
+
         return relevant_docs
